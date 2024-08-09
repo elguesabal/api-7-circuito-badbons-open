6a4c20cb419abc172a5defd8b43254d815dd546d
elguesabal <joseelguesabal@gmail.com>
2024-08-08 22:43:45 -0300

    limpando comentarios

--- index.js
+++ index.js
@@ -11,40 +11,42 @@ app.use(cors());
 app.get('/', (req, res) => res.send("teste"));
 
 app.get("/tabela", async (req, res) => {
-    try {
-        const categoria = req.query.categoria;
-        const modalidade = req.query.modalidade;
-        const data = await readJsonFile(pathFile(`../tabelas/${categoria}/${modalidade}.json`));
-        res.json(data.tabela);
-    } catch (error) {
-        res.send("Erro ao encontrar tabela.");
-    }
+	try {
+		const categoria = req.query.categoria;
+		const modalidade = req.query.modalidade;
+		const data = await readJsonFile(pathFile(`../tabelas/${categoria}/${modalidade}.json`));
+
+		res.json(data.tabela);
+	} catch (error) {
+		res.send("Erro ao encontrar tabela.");
+	}
 });
 
 app.get("/jogos", async (req, res) => {
-    try {
-        const categoria = req.query.categoria;
-        const modalidade = req.query.modalidade;
-        const data = await readJsonFile(pathFile(`../jogos/${categoria}/${modalidade}.json`));
-        res.json(data.jogos);
-    } catch (error) {
-        res.send("Erro ao encontrar tabela.");
-    }
+	try {
+		const categoria = req.query.categoria;
+		const modalidade = req.query.modalidade;
+		const data = await readJsonFile(pathFile(`../jogos/${categoria}/${modalidade}.json`));
+
+		res.json(data.jogos);
+	} catch (error) {
+		res.send("Erro ao encontrar tabela.");
+	}
 });
 
 app.put("/adm/jogos/:categoria/:modalidade", async (req, res) => {
-    const categoria = req.params.categoria;
-    const modalidade = req.params.modalidade;
-    const jogos = { jogos: req.body };
-    const tabela = { tabela: updateTable(await readJsonFile(pathFile(`../tabelas/${categoria}/${modalidade}.json`)), req.body) };
+	try {
+		const categoria = req.params.categoria;
+		const modalidade = req.params.modalidade;
+		const jogos = { jogos: req.body };
+		const tabela = { tabela: updateTable(await readJsonFile(pathFile(`../tabelas/${categoria}/${modalidade}.json`)), req.body) };
 
-    try {
-        await writeJsonFile(pathFile(`../jogos/${categoria}/${modalidade}.json`), jogos);
-        await writeJsonFile(pathFile(`../tabelas/${categoria}/${modalidade}.json`), tabela);
-        res.send("ok");
-    } catch (error) {
-        res.send("Erro ao gravar tabela.");
-    }
+		await writeJsonFile(pathFile(`../jogos/${categoria}/${modalidade}.json`), jogos);
+		await writeJsonFile(pathFile(`../tabelas/${categoria}/${modalidade}.json`), tabela);
+		res.status(200).send("ok");
+	} catch (error) {
+		res.status(500).send("Erro ao gravar tabela.");
+	}
 });
 
 
--- src/functionJson.js
+++ src/functionJson.js
@@ -2,32 +2,14 @@ import { promises as fs } from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// // // Obter caminho do arquivo JSON
-// const __filename = fileURLToPath(import.meta.url);
-// // // console.log("aaaaaaaa", __filename)
-// const __dirname = path.dirname(__filename);
-// // // console.log("bbbbbbbb", __dirname)
-// const jsonFilePath = path.join(__dirname, '../data.json');
-// // console.log("cccccccc", jsonFilePath)
-
 export function pathFile(file) {
 	const filename = fileURLToPath(import.meta.url);
 	const dirname = path.dirname(filename);
 	return path.join(dirname, file);
 }
 
-
-// "tabela": [
-// 	{ "nome": "Felipe Santos", "tj": "0", "v": "1", "d": "0", "vSet": "0", "pf": "0", "ps": "0" },
-// 	{ "nome": "Gabriel Monteiro", "tj": "0", "v": "4", "d": "0", "vSet": "0", "pf": "0", "ps": "0" },
-// 	{ "nome": "João Felipe", "tj": "0", "v": "2", "d": "0", "vSet": "0", "pf": "0", "ps": "0" },
-// 	{ "nome": "Matheus Petry", "tj": "0", "v": "5", "d": "0", "vSet": "0", "pf": "0", "ps": "0" }
-// ]
-
 export function updateTable(tabela, jogos) {
 	let newTabela = [];
-	// console.log("antiga tabela: ", tabela.tabela.length)
-	// console.log(jogos)
 
 	for(let i = 0; i < tabela.tabela.length; i++) {
 		let atleta = { nome: tabela.tabela[i].nome, tj: 0, v: 0, d: 0, vSet: 0, pf: 0, ps: 0 };
@@ -50,31 +32,21 @@ export function updateTable(tabela, jogos) {
 		atleta.tj = atleta.v + atleta.d; // tj -> total de jogos disputados
 		newTabela.push(atleta);
 	}
-	// console.log("newTabela", newTabela)
 	return newTabela;
 }
 
-// console.log("kkkkkkkk:", pathFile('../data.json'))
-
-
-// Função para ler o arquivo JSON
 export async function readJsonFile(jsonFilePath) {
-// console.log("leu:", jsonFilePath)
 	try {
-	//   const data = await fs.readFile(jsonFilePath, 'utf8');
-// console.log("leu:", data)
-	//   return JSON.parse(data);
-	  return JSON.parse(await fs.readFile(jsonFilePath, 'utf8'));
+		return JSON.parse(await fs.readFile(jsonFilePath, 'utf8'));
 	} catch (err) {
-	  throw new Error('Erro ao ler o arquivo JSON');
+		throw new Error('Erro ao ler o arquivo JSON');
 	}
 };
-  
-  // Função para escrever no arquivo JSON
+
 export async function writeJsonFile(jsonFilePath, data) {
 	try {
-	  await fs.writeFile(jsonFilePath, JSON.stringify(data, null, 2));
+		await fs.writeFile(jsonFilePath, JSON.stringify(data, null, 2));
 	} catch (err) {
-	  throw new Error('Erro ao escrever no arquivo JSON');
+		throw new Error('Erro ao escrever no arquivo JSON');
 	}
 };
